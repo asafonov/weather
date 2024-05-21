24e2c7271ff2a28268f012fb7c437e76a9d547f0
asafonov <me@asafonov.org>
2024-05-21 11:52:21 +0000

    improvements

--- models/forecast.js
+++ models/forecast.js
@@ -12,17 +12,53 @@ class Forecast {
     return this.place
   }
 
+  formatData (item) {
+    return {
+      temp: item.temp,
+      time: item.date.substr(11),
+      hour: item.date.substr(11, 2),
+      day: item.date.substr(8, 2),
+      wind_speed: item.wind_speed,
+      wind_direction: item.wind_direction,
+      pressure: item.pressure,
+      humidity: item.humidity,
+      clouds: item.clouds,
+      rain: item.rain,
+      snow: item.snow
+    }
+  }
+
   async getData() {
-    let data = asafonov.cache.get(place)
+    let data = asafonov.cache.get(this.place)
 
     if (! data) {
-      const url = `${asafonov.settings.apiUrl}/?place=${place}`
-      apiResp = await fetch(url)
+      const url = `${asafonov.settings.apiUrl}/?place=${this.place}`
+      try {
+        const apiResp = await (await fetch(url)).json()
+        const date = apiResp[0].date.substr(0, 10)
+        let maxToday = apiResp[0].temp
+        let minToday = apiResp[0].temp
 
-      data = {
-        now: {
-          temp: apiResp[0].temp
+        for (let i = 1; i < apiResp.length; ++i) {
+          let d = apiResp[i].date.substr(0, 10)
+          let h = apiResp[i].date.substr(12, 2)
+
+          if (d === date) {
+            maxToday = Math.max(apiResp[i].temp, maxToday)
+            minToday = Math.min(apiResp[i].temp, minToday)
+          }
         }
+
+        data = {
+          now: {
+            ...this.formatData(apiResp[0]), ...{max: maxToday, min: minToday}
+          }
+        }
+
+        console.log(data)
+      } catch (e) {
+        console.error(e)
+        return
       }
     }
 
--- view/forecast.js
+++ view/forecast.js
@@ -1,16 +1,40 @@
 class ForecastView {
+
   constructor (place) {
     this.model = new Forecast(place)
-    this.cityName = document.querySelector('.city_name')
   }
 
-  display() {
-    this.cityName.innerHTML = this.model.getPlace()
+  getIconByData (data) {
+    const icons = []
+    icons.push(data.clouds > 75 ? 'cloud' : data.hour >= '20' || data.hour <= '08' ? 'moon' : 'sun')
+
+    if (data.clouds >= 25 && data.clouds <= 75) icons.push('cloud')
+    if (data.rain) icons.push('rain')
+    if (data.wind_speed > 8) icons.push('wind')
+    if (icons.length > 1 && icons[0] === 'cloud') icons[0] = 'cloud_with' 
+
+    return icons
+  }
+
+  async display() {
+    document.querySelector('.city_name').innerHTML = this.model.getPlace()
+    const data = await this.model.getData()
+
+    if (! data) return
+
+    document.querySelector('.temperature .now').innerHTML = `${data.now.temp}°`
+    document.querySelector('.temperature .max').innerHTML = `${data.now.max}°`
+    document.querySelector('.temperature .min').innerHTML = `${data.now.min}°`
+    document.querySelector('.city_time').innerHTML = data.now.time
+    const icons = this.getIconByData(data.now)
+    const iconDiv = document.querySelector('.icon_weather')
+    iconDiv.innerHTML = `<svg class="icon_with"><use xlink:href="#${icons[0]}" /></svg>`
+    if (icons.length > 1) iconDiv.innerHTML += `<svg class="icon_dop"><use xlink:href="#${icons[1]}" /></svg>`
+    if (icons.length > 2) iconDiv.innerHTML += `<svg class="icon_dop dop_second dop_duo"><use xlink:href="#${icons[2]}" /></svg>`
   }
 
   destroy() {
     this.model.destroy()
     this.model = null
-    this.cityName = null
   }
 }
