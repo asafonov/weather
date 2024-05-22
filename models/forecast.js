5cdf3c4de41c1b562ce962cac1e29d454280a5c7
asafonov <me@asafonov.org>
2024-05-22 12:17:58 +0000

    daily forecast

--- models/forecast.js
+++ models/forecast.js
@@ -32,21 +32,52 @@ class Forecast {
     let data = asafonov.cache.get(this.place)
 
     if (! data) {
-      data = {hourly: []}
+      data = {hourly: [], daily: []}
       const url = `${asafonov.settings.apiUrl}/?place=${this.place}`
       try {
         const apiResp = await (await fetch(url)).json()
         const date = apiResp[0].date.substr(0, 10)
         let maxToday = apiResp[0].temp
         let minToday = apiResp[0].temp
+        let prevDate = date
 
         for (let i = 1; i < apiResp.length; ++i) {
           let d = apiResp[i].date.substr(0, 10)
-          let h = apiResp[i].date.substr(12, 2)
+          let h = apiResp[i].date.substr(11, 2)
+
+          if (d !== prevDate) {
+            if (data.daily.length > 0) {
+              const index = data.daily.length - 1
+              if (data.daily[index].rain < 0.5) data.daily[index].rain = 0
+              if (data.daily[index].snow < 0.5) data.daily[index].snow = 0
+            }
+
+            data.daily.push({rain: 0, snow: 0, clouds: 0, wind_speed: 0, day: apiResp[i].day})
+            prevDate = d
+          }
 
           if (d === date) {
             maxToday = Math.max(apiResp[i].temp, maxToday)
             minToday = Math.min(apiResp[i].temp, minToday)
+          } else {
+            const index = data.daily.length - 1
+            data.daily[index].rain = Math.max(apiResp[i].rain || 0, data.daily[index].rain)
+            data.daily[index].snow = Math.max(apiResp[i].snow || 0, data.daily[index].snow)
+            data.daily[index].wind_speed = Math.max(apiResp[i].wind_speed || 0, data.daily[index].wind_speed)
+            data.daily[index].clouds += (apiResp[i].clouds || 0) / 8
+
+            if (h >= '02' && h < '05') {
+              data.daily[index].morning = apiResp[i].temp
+            }
+
+            if (h > '12' && h <= '15') {
+              data.daily[index].temp = apiResp[i].temp
+              data.daily[index].wind_direction = apiResp[i].wind_direction
+            }
+
+            if (h >= '21') {
+              data.daily[index].evening = apiResp[i].temp
+            }
           }
 
           if (i <= 16) {
--- view/forecast.js
+++ view/forecast.js
@@ -10,7 +10,7 @@ class ForecastView {
 
     if (! data.rain && ! data.snow && data.clouds >= 25 && data.clouds <= 75) icons.push('cloud')
     if (data.rain) icons.push('rain')
-    if (data.rain > 2.5) icons.push('rain')
+    if (data.rain > 1) icons.push('rain')
     if (data.wind_speed > 8) icons.push('wind')
     if (icons.length > 1 && icons[0] === 'cloud') icons[0] = 'cloud_with'
 
@@ -22,6 +22,11 @@ class ForecastView {
     if (icons.length > 1) ret += `<svg class="icon_dop"><use xlink:href="#${icons[1]}" /></svg>`
     if (icons.length > 2) ret += `<svg class="icon_dop dop_second dop_duo"><use xlink:href="#${icons[2]}" /></svg>`
     return ret
+  }
+
+  getDayName (day) {
+    const dayNames = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    return dayNames[day - 1]
   }
 
   async display() {
@@ -50,6 +55,49 @@ class ForecastView {
             ${this.getIcon(this.getIconByData(data.hourly[i]))}
           </div>
           <div class="text_h3">${data.hourly[i].temp}°</div>
+        </div>`
+    }
+
+    const dailyDiv = document.querySelector('.days_list')
+    dailyDiv.innerHTML = ''
+
+    for (let i = 0; i < data.daily.length; ++i) {
+      if (! data.daily[i].evening) break
+
+      dailyDiv.innerHTML +=
+        `<div class="item_days_list flex_row centered">
+          <div class="day_name">${i === 0 ? 'Tomorrow' : this.getDayName(data.daily[i].day)}</div>
+          <div class="right_part flex_row centered">
+            <div class="icon icon_weather icon_normal${data.daily[i].rain ? '' : ' icon_dop_top'}">
+              ${this.getIcon(this.getIconByData(data.daily[i]))}
+            </div>
+            <div class="temperature flex_row">
+              <div class="text_accent">${data.daily[i].morning}°</div>
+              <div class="icon icon_small">
+                <svg>
+                  <use xlink:href="#sun_up"/>
+                </svg>
+              </div>
+              <div class="text_h3">${data.daily[i].temp}°</div>
+              <div class="icon icon_small">
+                <svg>
+                  <use xlink:href="#sun_down"/>
+                </svg>
+              </div>
+              <div class="text_accent">${data.daily[i].evening}°</div>
+            </div>
+            <div class="wind flex_row centered">
+              <div class="power">${data.daily[i].wind_speed}</div>
+              <div class="direction flex_col centered">
+                <div class="icon icon_fill icon_compas compas_se">
+                  <svg>
+                    <use xlink:href="#direction"/>
+                  </svg>
+                </div>
+                <div class="text_small_dop">${data.daily[i].wind_direction}</div>
+              </div>
+            </div>
+          </div>
         </div>`
     }
   }
