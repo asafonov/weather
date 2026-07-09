56a097c39c7f7ca6a9261eba5f9e79a9e49e4668
asafonov <me@asafonov.org>
2026-07-09 12:07:30 +0000

    minor fix for request url

--- models/forecast.js
+++ models/forecast.js
@@ -52,7 +52,7 @@ class Forecast {
 
     if (! data) {
       data = {hourly: [], daily: []}
-      const url = `${asafonov.settings.apiUrl}/?` + (this.place ? `place=${this.place}` : `lat=${this.lat}&lon=${this.lon}`)
+      const url = `${asafonov.settings.apiUrl}?` + (this.place ? `place=${this.place}` : `lat=${this.lat}&lon=${this.lon}`)
 
       try {
         const apiResp = await asafonov.request.get(url, true)
